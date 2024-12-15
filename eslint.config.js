7bdc25179a81f53ff03be2e1b6f385b832279df2
Pyroborn <152436292+Pyroborn@users.noreply.github.com>
2024-12-15 12:29:22 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      // Define which global variables to include
+      globals: {
+        ...globals.browser, // This includes browser-specific globals like `window`, `document`, etc.
+      },
+    },
+  },
+  pluginJs.configs.recommended, // Using the recommended configuration from ESLint plugin for JavaScript
+  ...pluginVue.configs["flat/essential"], // Using Vue's essential configuration
 ];
